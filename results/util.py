43d1d165d979b0fc2b4b4d465f24c71adc3b6329
Carolyn Hicks <chicks@actu.org.au>
2020-05-01 16:10:20 +1000

    remove dependency on py

--- results/util.py
+++ results/util.py
@@ -1,10 +1,8 @@
 from pathlib import Path
 
-from py._path.local import LocalPath
-
 
 def is_pathlike(f):
-    return isinstance(f, (str, Path, LocalPath))
+    return isinstance(f, (str, Path))
 
 
 def noneslarger(x):
